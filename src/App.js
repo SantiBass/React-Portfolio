5b30e420a04118eca1b3ad5d79953a3d8372eb5b
SantiBass <sibarraryan@gmail.com>
2022-02-21 18:00:47 -0800

    working on the pictures for the business gallery

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import logo from './logo.svg';
 import About from './components/About/about';
 import Nav from './components/Nav/nav';
@@ -6,9 +6,23 @@ import Business from './components/Business/business';
 import './App.css';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+  ])
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
   return (
     <div >
-    <Nav></Nav>
+    <Nav>
+      categories = {categories}
+      setCurrentCategory= {setCurrentCategory}
+      currentCategory={currentCategory}
+    </Nav>
    <main>
      <Business></Business>
      <About></About>
--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -1,14 +1,24 @@
-import React from 'react'
-
-function Business() {
+import React from 'react';
+import {capitalizeFirstLetter} from '../../utils/helpers';
+import photo from '../../assets/small/commercial/0.jpg'
+function Business(props) {
+    const currentCategory = {
+        name: "Business",
+        description:"Photos of preview projects"
+    };
   return (
     <section>
         <h1>
-            Name
+           {capitalizeFirstLetter(currentCategory.name)}
         </h1>
         <p>
-            Description
+            {currentCategory.name}
         </p>
+        <div className='flex-row'>
+            <img src= {photo} alt= 'Horiseon website'
+           className='img-thumgnail mx-1'
+           />
+        </div>
     </section>
   )
 }
