7c8aecbc29b2f722c3f40ba65dc44701a3c18951
SantiBass <sibarraryan@gmail.com>
2022-02-22 08:03:12 -0800

    working rollback

--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -1,26 +1,16 @@
 import React from 'react';
+import Photolist from '../../Photolist/photolist';
 import {capitalizeFirstLetter} from '../../utils/helpers';
-import photo from '../../assets/small/commercial/0.jpg'
-function Business(props) {
-    const currentCategory = {
-        name: "Business",
-        description:"Photos of preview projects"
-    };
-  return (
-    <section>
-        <h1>
-           {capitalizeFirstLetter(currentCategory.name)}
-        </h1>
-        <p>
-            {currentCategory.name}
-        </p>
-        <div className='flex-row'>
-            <img src= {photo} alt= 'Horiseon website'
-           className='img-thumgnail mx-1'
-           />
-        </div>
-    </section>
-  )
-}
-
-export default Business
+// import photo from '../../assets/small/commercial/0.jpg'
+function Business({ currentCategory }) {
+    const { name, description } = currentCategory;
+    return (
+      <section>
+        <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
+        <p>{description}</p>
+        <Photolist/>
+      </section>
+    );
+  }
+  
+  export default Business;
