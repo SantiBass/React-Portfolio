d6bb7300dcc52a7e9625ab74ec03d9bc39e5a71a
SantiBass <sibarraryan@gmail.com>
2022-02-24 19:56:18 -0800

    solved the footer links

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,13 +14,13 @@ function Home() {
       <div className='welcome pageHeight'>
 
         Santiago Ibarra Ryan.
-        <ul style={{ justifyContent: 'center' }} className='flex-row '>
+        <ul style={{ justifyContent: 'left' }} className='flex-row '>
           <li className='hook'>Learning Never Stops</li>
           <li className='hook'>Web Developer</li>
           <li className='hook'>Musician</li>
         </ul>
         <div >
-          <ul style={{ justifyContent: 'center' }} className='flex-row '>
+          <ul style={{ justifyContent: 'left' }} className='flex-row '>
             <li className='hook'>
               <button className='button large  is-rounded'> <a href={links.gitHub} without rel="noopener noreferrer" target="_blank">GitHub</a>
                 <GitHubIcon style={{ width: '100px', height: "50px" }} />
